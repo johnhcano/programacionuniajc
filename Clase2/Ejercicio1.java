3f940ec98ac884b471241388e1bbad1eb33d2a47
JhonHCanoB <jhon.canob@campusucc.edu.co>
2025-02-11 17:28:05 +0000

    Se gregó el metodo de llenar el arreglo con números aleatorios

--- Clase2/Ejercicio1.java
+++ Clase2/Ejercicio1.java
@@ -3,7 +3,7 @@ public class Ejercicio1 {
         
         //Creación del objeto op de la clase OperacionesConArreglos
         OperacionesConArreglos op = new OperacionesConArreglos();
-        
+        /* 
         // Crear un arreglo de enteros
         int[] a = {10, 20, 33, 24, 15, 26, 67, 28, 9, 10};
 
@@ -11,5 +11,16 @@ public class Ejercicio1 {
         System.out.println(op.mostrarElementos(a));
         System.out.println("La suma de los elementos del arreglo es: " + op.sumarElementos(a));
         System.out.println("El promedio de los elementos del arreglo es: " + op.promedioElementos(a));
+        */
+
+        int n = 5;
+        int[] a = op.llenarArregloAleatorios(n);
+        System.out.println(op.mostrarElementos(a));
+
+        //Probando la generación de números aleatorios entre un rango de valores
+        int min = -10;
+        int max = 20;
+        int randomNum = min + (int)(Math.random() * ((max - min) + 1));
+        System.out.println("Número aleatorio generado entre " + min + " y " + max + ": " + randomNum);
     }
 }
--- Clase2/OperacionesConArreglos.java
+++ Clase2/OperacionesConArreglos.java
@@ -25,4 +25,12 @@ public class OperacionesConArreglos {
         return promedio;
     }
 
+    public int[] llenarArregloAleatorios(int tam) {
+        int[] a = new int[tam];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = (int) (Math.random() * 100);
+        }
+        return a;
+    }
+
 }
