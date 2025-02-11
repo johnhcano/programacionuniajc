33e50232422529041c68a0bbddfe93570941a73b
JhonHCanoB <jhon.canob@campusucc.edu.co>
2025-02-11 16:52:41 +0000

    metodo

--- Clase2/Ejercicio1.java
+++ Clase2/Ejercicio1.java
@@ -1,13 +1,13 @@
 public class Ejercicio1 {
     public static void main(String[] args) {
         
+        //Creación del objeto op de la clase OperacionesConArreglos
         OperacionesConArreglos op = new OperacionesConArreglos();
         
         // Crear un arreglo de enteros
         int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
+        // Mostrar los elementos del arreglo
         System.out.println(op.mostrarElementos(a));
-
-        
     }
 }
